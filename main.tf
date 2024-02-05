0089368640ac5ac35db35456174fea00cd8a33c9
Moin Ghori <moinghori.it@gmail.com>
2024-02-04 22:25:02 -0600

    Static Website hosting on AWS S3 bucket

---
+++ main.tf
@@ -0,0 +1,65 @@
+resource "aws_s3_bucket" "mg-bucket-103" {
+  bucket = var.proj1-s3-bucket
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership-control" {
+  bucket = aws_s3_bucket.mg-bucket-103.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access-block" {
+  bucket = aws_s3_bucket.mg-bucket-103.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership-control,
+    aws_s3_bucket_public_access_block.public-access-block,
+  ]
+
+  bucket = aws_s3_bucket.mg-bucket-103.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mg-bucket-103.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+  acl = "public-read"
+
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mg-bucket-103.id
+  key    = "error.html"
+  source = "error.html"
+  content_type = "text/html"
+  acl = "public-read"
+
+}
+
+resource "aws_s3_bucket_website_configuration" "ws-config" {
+  bucket = aws_s3_bucket.mg-bucket-103.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [aws_s3_bucket_acl.acl ]
+}
+
+
+
+
---
+++ variable.tf
@@ -0,0 +1,3 @@
+variable "proj1-s3-bucket" {
+	default = "mg-bucket-103"
+}
